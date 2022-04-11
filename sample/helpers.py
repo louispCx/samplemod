76e65b02a15998a80267233ff4960d6db4c3adc1
louispCx <99268939+louispCx@users.noreply.github.com>
2022-04-11 11:26:05 +0900

    Update helpers.py

--- sample/helpers.py
+++ sample/helpers.py
@@ -1,3 +1,11 @@
 def get_answer():
     """Get an answer."""
     return True
+
+import requests
+
+
+def get_btc_usd_value():
+    r = requests.get('https://cex.io/api/last_price/BTC/USD')
+    r.raise_for_status()
+    return r.json()
